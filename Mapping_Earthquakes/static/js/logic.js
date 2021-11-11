35a7e8fd34b8da881578f6465035997ef8ac6b7c
Jin <jin6621@berkeley.edu>
2021-11-11 11:34:21 -0800

    adding majorAirports.json

--- Mapping_Earthquakes/static/js/logic.js
+++ Mapping_Earthquakes/static/js/logic.js
@@ -5,7 +5,7 @@ let cityData = cities;
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+let map = L.map('mapid').setView([37.5, -122.5], 10);
 
 // Coordinates for each point to be used in the line.
 let line = [
@@ -14,6 +14,47 @@ let line = [
     [40.7899, -111.9791],
     [47.4502, -122.3088]
 ];
+
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{"type":"Feature", "properties":{
+                                                                        "id":"3469",
+                                                                        "name":"San Francisco International Airport",
+                                                                        "city":"San Francisco",
+                                                                        "country":"United States",
+                                                                        "faa":"SFO",
+                                                                        "icao":"KSFO",
+                                                                        "alt":"13",
+                                                                        "tz-offset":"-8",
+                                                                        "dst":"A",
+                                                                        "tz":"America/Los_Angeles"},
+                                                            "geometry":{
+                                                                "type":"Point",
+                                                                "coordinates":[-122.375,37.61899948120117]}}
+]};
+
+// //Grabbing our GeoJSON data.
+// L.geoJson(sanFranAirport, {
+//     pointToLayer: function(feature, latlng) {
+//         console.log(feature)
+//         return L.marker(latlng).bindPopup("<h4>" + feature.properties.name + "</h4><hr></3>" + feature.properties.city + ", " + feature.properties.country + "</h3>");
+//     }
+// }).addTo(map);
+// L.geoJSON(sanFranAirport).addTo(map);
+
+// L.geoJson(sanFranAirport, {
+//     onEachFeature: function(feature, layer) {
+//         console.log(layer);
+//         layer.bindPopup("<h4>" + feature.properties.name + "</h4>").addTo(map);
+//     }
+// });
+
+L.geoJson(sanFranAirport, {
+    onEachFeature: function(feature, layer) {
+        console.log(layer);
+        layer.bindPopup("<h4>" + feature.properties.name + "</h4>").addTo(map);
+    }
+});
 
 // Create a polyline using the line coordinates and make the line red.
 L.polyline(line, {
@@ -30,12 +71,13 @@ L.polyline(line, {
 // });
 
 // Loop through the cities array and create one marker for each city.
-cityData.forEach(function(city) {
-    console.log(city)
-    L.circleMarker(city.location, {
-        radius: city.population/100000
-    }).bindPopup("<h2>" + city.city + ", " + city.state + "</h2><hr><h3> Population: " + city.population.toLocaleString() + "<h3>").addTo(map);
-});
+// cityData.forEach(function(city) {
+//     console.log(city)
+//     L.circleMarker(city.location, {
+//         radius: city.population/100000,
+//         fillOpacity: 0.35,
+//     }).bindPopup("<h2>" + city.city + ", " + city.state + "</h2><hr><h3> Population: " + city.population.toLocaleString() + "<h3>").addTo(map);
+// });
 
 // // Add a circle to the map for Los Angeles, California.
 // L.circle(cities,{
@@ -45,7 +87,7 @@ cityData.forEach(function(city) {
 // }).addTo(map);
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}'
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-night-v1/tiles/{z}/{x}/{y}?access_token={accessToken}'
 , {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
