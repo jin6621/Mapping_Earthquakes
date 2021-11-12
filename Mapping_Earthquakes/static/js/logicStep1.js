29d7fb15e65eb213c9de82e765e2716917f58433
Jin <jin6621@berkeley.edu>
2021-11-11 16:22:20 -0800

    Uploading Earthquakes_past7days

--- Mapping_Earthquakes/static/js/logicStep1.js
+++ Mapping_Earthquakes/static/js/logicStep1.js
@@ -125,7 +125,13 @@ streets.addTo(map);
 // Accessing the airport GeoJSON URL
 //let airportData = "https://raw.githubusercontent.com/jin6621/Mapping_Earthquakes/Mapping_GeoJSON_Points/Mapping_Earthquakes/majorAirports.json";
 //let torontoData = "https://raw.githubusercontent.com/jin6621/Mapping_Earthquakes/Mapping_GeoJSON_Linestrings/Mapping_Earthquakes/torontoRoutes.json";
-let torontoHoods = "https://raw.githubusercontent.com/jin6621/Mapping_Earthquakes/Mapping_GeoJSON_Polygons/Mapping_Earthquakes/torontoNeighborhoods.json";
+//let torontoHoods = "https://raw.githubusercontent.com/jin6621/Mapping_Earthquakes/Mapping_GeoJSON_Polygons/Mapping_Earthquakes/torontoNeighborhoods.json";
+
+// Retrieve the earthquake GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+  // Creating a GeoJSON layer with the retrieved data.
+    L.geoJson(data).addTo(map);
+});
 
 // Create a style for the lines
 let myStyle = {
@@ -155,12 +161,12 @@ let myStyle = {
 // )};
 
 // Grabbing our GeoJSON data.
-d3.json(torontoHoods).then(function(data){
-    console.log(data);
+// d3.json(torontoHoods).then(function(data){
+//     console.log(data);
 
-    L.geoJson(data,{
-        style: myStyle,
-        onEachFeature: function(feature, layer){
-            console.log(layer);
-            layer.bindPopup("<h3>Neighborhood: " + feature.properties.AREA_NAME + "</h3>").addTo(map);
-}})});
+//     L.geoJson(data,{
+//         style: myStyle,
+//         onEachFeature: function(feature, layer){
+//             console.log(layer);
+//             layer.bindPopup("<h3>Neighborhood: " + feature.properties.AREA_NAME + "</h3>").addTo(map);
+// }})});
